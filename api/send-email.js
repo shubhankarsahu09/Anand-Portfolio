7395ec4c7770f573249d3af1204f57c3ecf8355a
shubhankarsahu09 <shubhankarsahu09@gmail.com>
2026-06-08 11:38:12 +0530

    feat: add package.json with ESM configuration and revert api to ESM

--- api/send-email.js
+++ api/send-email.js
@@ -1,4 +1,4 @@
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
     // Enable CORS
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
